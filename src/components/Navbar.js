86cfb801d5cc28b82e233a610c858e24faeba993
Vivek Kadam <vk2567298@gmail.com>
2024-08-15 21:19:00 +0530

    hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,36 @@
-import React from "react"
-import { NavLink, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react"
+import {  useNavigate } from "react-router-dom";
 import resume from "../assets/resume.pdf";
-const Navbar = (props) => {
+import { GiHamburgerMenu } from "react-icons/gi";
+
+ const Navbar = (props) => {
     const navigator = useNavigate();
+    const [display,setDisplay]=useState(true);
+    function burgerHandler(){
+           setDisplay(!display); // Toggling the display.
+           
+            
+    }
+
+    useEffect(()=>{
+       if(window.innerWidth<941){
+        setDisplay(false);
+       }
+       
+    },[window.innerWidth<941]);
+
+
+     
+    
+    
     return (
-        <div className="bg-black   w-screen min-w-[360px] shadow-md overflow-y-hidden ">
-            <div className=" flex w-10/12 min-w-[360px] py-5 mx-auto justify-between  px-10 flex-wrap items-center gap-4 ">
+        <div className="bg-black   w-screen   shadow-md   fixed">
+            <div className={` flex w-10/12 min-w-[360px] py-5 mx-auto justify-between  px-10  items-center gap-4 navbar-res    ${display ? "flex ":"hidden bg-transparent"} transition-all duration-200`}>
                 <h2 className="font-bold text-2xl text-white cursor-pointer" onClick={() => {
                     navigator('/')
                 }}>PORT<span className="text-[#ff004f]">FOLIO</span></h2>
                 <div>
-                    <div className="text-white flex gap-6  items-center text-xl flex-wrap">
+                    <div className="text-white flex gap-6  items-center text-xl navbar-res">
                         <button onClick={() => {
                             navigator('/');
                         }} className="hover:bg-red-500 hover:text-black rounded-md px-2 py-1">Home</button>
@@ -19,7 +39,7 @@ const Navbar = (props) => {
                             navigator("/about")
                         }} className="hover:bg-red-500 hover:text-black rounded-md px-2 py-1">About me</button>
 
-                        <button className="hover:bg-red-500 hover:text-black rounded-md px-2 py-1"> <a href={resume} target="_blank" className="border-none p-none">Resume</a></button>
+                        <button className="hover:bg-red-500 hover:text-black rounded-md px-2 py-1"> <a href={resume} target="_blank" rel="noreferrer" className="border-none p-none">Resume</a></button>
                         
                         <button className="hover:bg-red-500 hover:text-black rounded-md px-2 py-1" onClick={()=>{
                             navigator('/projects')
@@ -30,6 +50,11 @@ const Navbar = (props) => {
                     </div>
                 </div>
             </div>
+          <div className="absolute right-2 top-2   gap-1  hidden burger-res cursor-pointer text-white " onClick={burgerHandler}>
+          <GiHamburgerMenu />
+
+
+            </div>            
         </div>
     )
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ const Projects = (props) => {
         <div className="bg-black h-screen w-screen flex justify-center items-center ">
             <div className="">
                 <iframe   src="https://www.youtube.com/embed/zAV-iP7xWrs?si=lHm0xZapvCHxW9aT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" className="min-w-[360px] min-h-[200px] max-w-[560px] max-h-[315px]" allowfullscreen></iframe>
-                <p className="text-white text-2xl font-semibold text-center">PathoCare: Pathology Management System </p>
+                <p className="text-green font-semibold text-center">PathoCare: Pathology Management System </p>
             </div>
 
         </div>
