d5db16432ba362b8554789c32c3c7f66a5889330
Vivek Kadam <vk2567298@gmail.com>
2024-08-15 22:50:01 +0530

    navbar edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = (props) => {
       
       <div className="relative -z-10   ">
         <div className="absolute -z-20  h-[100vh] ">
-          <img src={home} alt="home page" loading="eager" className="h-full w-screen " />
+          <img src={home} alt="home page" loading="eager" className="h-full w-[100vw] home-res" />
         </div>
         <div className="flex flex-col min-w-[360px] h-[100vh]  justify-center w-10/12 mx-auto gap-3  flex-wrap">
           <h2 className="text-5xl text-white">Hello, I'm</h2>
