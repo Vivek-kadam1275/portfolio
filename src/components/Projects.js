8c669650c2bac1c25768650623f873e1011dff41
Vivek Kadam <vk2567298@gmail.com>
2024-08-15 17:31:38 +0530

    video edit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,8 +4,8 @@ const Projects = (props) => {
     return (
         <div className="bg-black h-screen w-screen flex justify-center items-center ">
             <div className="">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/zAV-iP7xWrs?si=lHm0xZapvCHxW9aT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                <p>PathoCare: Pathology Management System </p>
+                <iframe   src="https://www.youtube.com/embed/zAV-iP7xWrs?si=lHm0xZapvCHxW9aT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" className="min-w-[360px] min-h-[200px] max-w-[560px] max-h-[315px]" allowfullscreen></iframe>
+                <p className="text-white text-2xl font-semibold text-center">PathoCare: Pathology Management System </p>
             </div>
 
         </div>
