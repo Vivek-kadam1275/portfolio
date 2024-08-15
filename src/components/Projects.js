08121826ec2c86bff595dd9bab4ea3642c4210a5
Vivek Kadam <vk2567298@gmail.com>
2024-08-15 22:05:47 +0530

    text color

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ const Projects = (props) => {
         <div className="bg-black h-screen w-screen flex justify-center items-center ">
             <div className="">
                 <iframe   src="https://www.youtube.com/embed/zAV-iP7xWrs?si=lHm0xZapvCHxW9aT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" className="w-[560px] h-[315px] project-res" allowfullscreen></iframe>
-                <p className="text font-semibold text-center">PathoCare: Pathology Management System </p>
+                <p className="text-white font-semibold text-center">PathoCare: Pathology Management System </p>
             </div>
 
         </div>
