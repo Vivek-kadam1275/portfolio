599a1520e96fe833a1ea3c85fbfc34a094a91a0f
Vivek Kadam <vk2567298@gmail.com>
2024-10-11 10:06:40 +0530

    video of pathocare

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,20 +5,20 @@ import ECOMZY from '../assets/ECOMZY.mp4';
 const Projects = (props) => {
     return (
         <div className="bg-[#181c31] w-screen  pt-20 overflow-y-scroll ">
-            
-            <div className="w-10/12  mx-auto flex gap-y-6 flex-wrap justify-around py-10">
+
+            <div className="w-10/12 min-w-[360px] mx-auto flex gap-y-6 flex-wrap justify-around py-10 project-container-res">
                 <div className="flex flex-col items-center gap-1">
                     {/* <video width="600" height="400" poster="" controls>
                         <source src={Pathocare} type="video/mp4" />
                         Your browser does not support the video tag.
                     
                     </video> */}
-                        <iframe src="https://www.youtube.com/embed/zAV-iP7xWrs?si=lHm0xZapvCHxW9aT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" className="w-[560px] h-[315px] project-res" allowfullscreen></iframe>
-                        
-                    <h2 className="text-white text-xl uppercase">PathoCare</h2>
+                    <iframe src="https://www.youtube.com/embed/zAV-iP7xWrs?si=lHm0xZapvCHxW9aT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"  width="560px" height="315px" className="project-res"/>
+
+                    <h2 className="text-white text-xl u">PathoCare</h2>
                 </div>
                 <div className="flex flex-col items-center gap-1">
-                    <video width="560" height="315" loadin controls>
+                    <video width="560" height="315" className="project-res" controls>
                         <source src={ECOMZY} type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
@@ -26,7 +26,7 @@ const Projects = (props) => {
                 </div>
 
                 <div className="flex flex-col items-center gap-1">
-                    <video  width="560" height="315" controls>
+                    <video width="560" height="315"className="project-res" controls>
                         <source src={weatherNow} type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
